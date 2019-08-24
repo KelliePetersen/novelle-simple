08a37aeecf91d1cbcb58b7a8fb57302dd1d00939
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-08-23 20:43:38 -0400

    add intro section and fix animated menu

--- app/src/js/modules/WidescreenMenu.js
+++ app/src/js/modules/WidescreenMenu.js
@@ -7,11 +7,20 @@ class WidescreenMenu {
   }
 
   events() {
+    window.addEventListener('scroll', this.hiddenMenu.bind(this));
     window.addEventListener('scroll', this.fixedMenu.bind(this));
   }
 
-  fixedMenu() {
+  hiddenMenu() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      this.nav[0].classList.add('nav--hidden');
+    } else {
+      this.nav[0].classList.remove('nav--hidden');
+    }
+  }
+
+  fixedMenu() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
       this.nav[0].classList.add('nav--fixed');
       this.navList[0].classList.add('nav__list--small');
       this.navLogo[0].classList.add('nav__logo--small');
