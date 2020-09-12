5c8fc61ecd5bc3cefc1e929edc06361715d4167d
KelliePetersen <kelliepetersen@outlook.com>
2020-09-12 18:06:30 +1000

    delete js files for scrollReveal

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,16 +1,7 @@
 import BackToTop from './modules/BackToTop';
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
 import WidescreenMenu from './modules/WidescreenMenu';
 
 const backToTop = new BackToTop();
 const mobileMenu = new MobileMenu();
-const revealOnScroll = new RevealOnScroll();
 const widescreenMenu = new WidescreenMenu();
-
-revealOnScroll.sr.reveal(".heading", { 
-  distance: "10px",
-  origin: "right",
-  delay: 600,
-  viewOffset: { bottom: -300 }
-});
